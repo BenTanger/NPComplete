f567223b48b44e46a6588c8057b78f1ea2101594
Logan-Page <lrpage9@gmail.com>
2025-11-20 13:55:05 -0500

    made file, ideas, an edited README

---
+++ ExactSolution.py
@@ -0,0 +1,4 @@
+# Make an inefficient solution that gives the exact right answer to the TPS problem
+# Maybe make algorithm recursive to get n!
+# Assume weights are all positive
+# Doesnt matter where you start, solution is the same or equivalent
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # NPComplete
 Swag Np Complete project
+
+
+Exact Architect:
+Cool Stuff
+
+
+Approximate Architect:
+Cooler Stuff
+
+
+Reduction Architect:
+Coolest Stuff
